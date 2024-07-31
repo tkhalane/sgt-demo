94525f645636bdfd930ce9a37a6dfcbea0bd7f5a
Tiisetso Khalane <tiisetsokhalane@Tiisetsos-MBP.lan>
2024-07-31 11:36:06 +0200

    major refactoring

--- platform/ui/insert-service-api.sh
+++ platform/ui/insert-service-api.sh
@@ -14,10 +14,10 @@ access_token=$(curl --location --request POST 'https://api.getport.io/v1/auth/ac
 blueprint_id='runningService'
 
 curl --location --request POST "https://api.getport.io/v1/blueprints/${blueprint_id}/entities?upsert=true" 	--header "Authorization: Bearer $access_token" 	--header "Content-Type: application/json" 	--data-raw '{
-	"identifier": "sgt_recharge",
-	"title": "sgt Recharge",
+	"identifier": "sgt_wealthbonus",
+	"title": "sgt wealthbonus",
 	"properties": {"locked":false,"version":"string","commitSha":"xssdewew","cpuLimit":1,"memoryLimit":40,"pullRequest":"https://github.com/tkhalane/sgt-demo/pull/2","namespace":"sgt-demo","grafana":"https://grafana.com","replicaCount":2,"cpu":1,"memory":30},
-	"relations": {"service":"sgt_recharge"}
+	"relations": {"service":"sgt_wealthbonus"}
 }'
 
 # The output of the command contains the content of the resulting blueprint
