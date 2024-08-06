4b688873f41ba85b224d51531779bcddc37cde32
Tiisetso Khalane <tiisetsokhalane@Tiisetsos-MBP.lan>
2024-08-06 21:46:35 +0200

    use resource instead of module

--- platform/terraform/rds/main.tf
+++ platform/terraform/rds/main.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "af-south-1"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-  filter {
-    name="region"
-    values = [ "af-south-1" ]
-  }
-}
 
 data "aws_vpc" "default_vpc" {
   id = var.vpc_id
@@ -17,7 +10,7 @@ data "aws_vpc" "default_vpc" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = data.aws_vpc.default_vpc
   cidr_block        = "10.0.1.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = "af-south-1a"
 
   tags = {
     "Name" = "private-subnet-1"
@@ -27,22 +20,31 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            =  data.aws_vpc.default_vpc
   cidr_block        = "10.0.2.0/24"
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = "af-south-1b"
 
   tags = {
     "Name" = "private-subnet-2"
   }
 }
+# module "rds_subnet_group" {
+#   source  = "terraform-aws-modules/rds/aws"
+#   version = "6.8.0"
 
+#   identifier        = "sgt-demo-subnetgroup"
+#   subnet_ids = [
+#     aws_subnet.private_subnet_1.id,
+#     aws_subnet.private_subnet_2.id
 
-module "rds_subnet_group" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "6.8.0"
-
-  identifier        = "sgt-demo-subnetgroup"
-  subnet_ids = [
+#     ]
+# }
+resource "aws_db_subnet_group" "sgt-demo-subnetgroup" {
+  name       = "sgt-demo-subnetgroup"
+  subnet_ids =  [
     aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id
+  ]
 
-    ]
+  tags = {
+    Name = "sgt-demo-subnetgroup"
+  }
 }
