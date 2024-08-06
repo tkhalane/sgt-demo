5f30b0f267d0e5a7cb5b1532fb8bfa65c568fba6
Tiisetso Khalane <tiisetsokhalane@Tiisetsos-MBP.lan>
2024-08-06 22:26:05 +0200

    fixed version in helper

--- platform/helpers/create-postgres.sh
+++ platform/helpers/create-postgres.sh
@@ -6,4 +6,4 @@ FILE_PATH=platform/resources/${NAME}-aws-postgres.yml
 cp platform/api/claim-postgres-aws-template.yml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
 yq --inplace ".spec.writeConnectionSecretToRef.name = \"${NAME}\"" $FILE_PATH
-yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
+yq --inplace ".spec.version = \"${VERSION}\"" $FILE_PATH
