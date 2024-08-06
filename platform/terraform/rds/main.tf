02298c8850119825f647caef5bf4ce66c314a69c
Tiisetso <tkhalane@gmail.com>
2024-08-06 22:01:51 +0200

    Update main.tf cidr ranges to match the vpc range

--- platform/terraform/rds/main.tf
+++ platform/terraform/rds/main.tf
@@ -9,7 +9,7 @@ data "aws_vpc" "default_vpc" {
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = data.aws_vpc.default_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "172.31.1.0/24"
   availability_zone = "af-south-1a"
 
   tags = {
@@ -19,7 +19,7 @@ resource "aws_subnet" "private_subnet_1" {
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            =  data.aws_vpc.default_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "172.31.2.0/24""
   availability_zone = "af-south-1b"
 
   tags = {
